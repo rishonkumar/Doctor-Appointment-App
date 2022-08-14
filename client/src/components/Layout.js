39b5744fa01fe7b189619c920ee31113eef7f1c0
rishonkumar <rishon.kumar.rahi@gmail.com>
2022-08-14 08:57:30 +0530

    Added Layout menu

--- client/src/components/Layout.js
+++ client/src/components/Layout.js
@@ -1,10 +1,62 @@
 import React from "react";
 import "../layout.css";
+import { Link, useLocation } from "react-router-dom";
+
 function Layout({ children }) {
+  const location = useLocation();
+  const userMenu = [
+    {
+      name: "Home",
+      path: "/",
+      icon: "ri-home-line",
+    },
+    {
+      name: "Appointments",
+      path: "/appointments",
+      icon: "ri-file-list-3-line",
+    },
+    {
+      name: "Apply Doctor",
+      path: "/apply-doctor",
+      icon: "ri-hospital-line",
+    },
+    {
+      name: "Profile",
+      path: "/profile",
+      icon: "ri-user-3-line",
+    },
+    {
+      name: "Logout",
+      path: "/logout",
+      icon: "ri-logout-box-r-line",
+    },
+  ];
+
+  const menuToBeRendered = userMenu;
+
   return (
     <div className="main">
       <div className="d-flex layout">
-        <div className="sidebar">Sidebar</div>
+        <div className="sidebar">
+          <div className="sidebar-header">
+            <h1>HA</h1>
+          </div>
+          <div className="menu">
+            {menuToBeRendered.map((menu) => {
+              const isActive = location.pathname === menu.path;
+              return (
+                <div
+                  className={`d-flex menu-item ${
+                    isActive && `active-menu-item`
+                  }`}
+                >
+                  <i className={menu.icon}></i>
+                  <Link to={menu.path}>{menu.name}</Link>
+                </div>
+              );
+            })}
+          </div>
+        </div>
         <div className="content">
           <div className="header">Header</div>
           <div className="body">{children}</div>
