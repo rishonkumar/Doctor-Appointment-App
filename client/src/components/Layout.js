5a75f5041e0836b037a1eecc9716398354bdf32b
rishonkumar <rishon.kumar.rahi@gmail.com>
2022-08-15 17:01:18 +0530

    Layout page updated

--- client/src/components/Layout.js
+++ client/src/components/Layout.js
@@ -1,8 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import "../layout.css";
 import { Link, useLocation } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 function Layout({ children }) {
+  // to hide side bar
+  const [collapsed, setCollapsed] = useState(false);
+  const { user } = useSelector((state) => state.user);
   const location = useLocation();
   const userMenu = [
     {
@@ -51,14 +55,35 @@ function Layout({ children }) {
                   }`}
                 >
                   <i className={menu.icon}></i>
-                  <Link to={menu.path}>{menu.name}</Link>
+                  {!collapsed && <Link to={menu.path}>{menu.name}</Link>}
                 </div>
               );
             })}
           </div>
         </div>
         <div className="content">
-          <div className="header">Header</div>
+          <div className="header">
+            {/* For collapsed => true*/}
+
+            {collapsed ? (
+              <i
+                className="ri-menu-2-line header-action-icon"
+                onClick={() => setCollapsed(false)}
+              ></i>
+            ) : (
+              <i
+                className="ri-close-line header-action-icon"
+                onClick={() => setCollapsed(true)}
+              ></i>
+            )}
+            {/* Notification icon */}
+            <div className="d-flex align-items-center px-4">
+              <i className="ri-notification-4-line header-action-icon px-3"></i>
+              <Link className="anchor" to="/" profile>
+                {user?.name}
+              </Link>
+            </div>
+          </div>
           <div className="body">{children}</div>
         </div>
       </div>
