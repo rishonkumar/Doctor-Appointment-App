6caf255db8c4a570ab0f399915b6f636a02db660
rishonkumar <rishon.kumar.rahi@gmail.com>
2022-08-16 11:39:10 +0530

    Added Doctor Schema

---
+++ models/doctorModel.js
@@ -0,0 +1,64 @@
+const mongoose = require("mongoose");
+const doctorSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+    },
+    website: {
+      type: String,
+      required: true,
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+    specialization: {
+      type: String,
+      required: true,
+    },
+    experience: {
+      type: String,
+      required: true,
+    },
+    feePerConsultation: {
+      type: Number,
+      required: true,
+    },
+    consultationHours: {
+      type: Number,
+      required: true,
+    },
+
+    fromTime: {
+      type: Number,
+      required: true,
+    },
+    toTime: {
+      type: Number,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const doctorModel = mongoose.model("doctors", doctorSchema);
+module.exports = doctorModel;
